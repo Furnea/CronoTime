33ed35df5cbb6e9f6c0c97d46dd64e6619472c69
Administrador <Administrador@2DAM01>
2023-12-12 20:59:06 +0100

    Inicio de proyecto

---
+++ src/cronotime/CronoTime.java
@@ -0,0 +1,10 @@
+package cronotime;
+
+public class CronoTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/cronotime/MiControlador.java
@@ -0,0 +1,5 @@
+package cronotime;
+
+public class MiControlador {
+
+}
