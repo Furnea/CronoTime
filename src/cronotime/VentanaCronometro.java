a78dd51adae03139e0fa33b5efb6c01963926050
Administrador <Administrador@2DAM01>
2023-12-12 21:20:04 +0100

    Eliminar main de ventana

--- src/cronotime/VentanaCronometro.java
+++ src/cronotime/VentanaCronometro.java
@@ -11,21 +11,7 @@ public class VentanaCronometro extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VentanaCronometro frame = new VentanaCronometro();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+
 
 	/**
 	 * Create the frame.
